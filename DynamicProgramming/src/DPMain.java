5a2a160b44bb43b76f7c5a3f41ec7225a639c724
Anjana Shankar <anjana.shankar@igp.com>
2016-04-28 09:33:41 +0530

    Dynamic Programming

---
+++ DynamicProgramming/src/DPMain.java
@@ -0,0 +1,17 @@
+
+public class DPMain {
+	public static void main(String[] args){
+//		callLIS();
+		callCoinChange();
+	}
+
+	private static void callCoinChange() {
+		CoinChange.coinChange();
+	}
+
+	private static void callLIS(){
+		int [] arr = {10, 22, 9, 33, 21, 50, 41, 60,80 };
+		int length = LIS.findLongestIncreasingSubsequence(arr);
+		System.out.println("Longest Increasing Subsequence is of length : "+length);
+	}
+}
